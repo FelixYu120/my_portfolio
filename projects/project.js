f7cbd247d883bc73225b286b2b61c84a4422b3ea
Felix Yu <felixyu2627@gmail.com>
2026-04-23 15:09:33 -0700

    Add project.js to fetch and render projects

---
+++ projects/project.js
@@ -0,0 +1,10 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+
+const projectsContainer = document.querySelector('.projects');
+
+if (projectsContainer) {
+    renderProjects(projects, projectsContainer, 'h2');
+}
+
