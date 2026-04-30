e452dc5211d1d62d79015488bf0b2279ba8770c1
Felix Yu <felixyu2627@gmail.com>
2026-04-30 13:23:05 -0700

    Integrate D3.js and create arc for projects visualization
    Added D3.js for arc generation and created an arc with full circle.

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,8 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 const projectsContainer = document.querySelector('.projects');
 
@@ -8,3 +10,7 @@ if (projectsContainer) {
     renderProjects(projects, projectsContainer, 'h2');
 }
 
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
