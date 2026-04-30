9089ced99983e05e4f37bc534f811362d95566c7
Felix Yu <felixyu2627@gmail.com>
2026-04-30 13:30:10 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -4,34 +4,22 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 async function init() {
     const projects = await fetchJSON('../lib/projects.json');
     const projectsContainer = document.querySelector('.projects');
+    renderProjects(projects, container, 'h2');
 
-    renderProjects(projects, projectsContainer, 'h2');
-    renderPieChart(projects);
-}
-
-function renderPieChart(projects) {
-    let rolledData = d3.rollups(
-        projects,
-        (v) => v.length,
-        (d) => d.year
-    );
-
-    let data = rolledData.map(([year, count]) => {
-        return { value: count, label: year };
-    });
-
+    let data = [1, 2]; 
+    let colors = ['gold', 'purple'];
+    
     let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-    let sliceGenerator = d3.pie().value((d) => d.value);
+    let sliceGenerator = d3.pie();
     let arcData = sliceGenerator(data);
-    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+    let arcs = arcData.map((d) => arcGenerator(d));
 
-    let svg = d3.select('#projects-pie-plot');
-    svg.selectAll('path').remove();
+    let svg = d3.select('svg');
 
-    arcData.forEach((d, idx) => {
+    arcs.forEach((arc, idx) => {
         svg.append('path')
-           .attr('d', arcGenerator(d))
-           .attr('fill', colors(idx));
+           .attr('d', arc)
+           .attr('fill', colors[idx]);
     });
 }
 
